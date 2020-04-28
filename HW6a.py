8affb3bdd2647fbd81f7ecd4b6868010fdabe257
Renata <hrr21@yandex.ru>
2020-04-28 12:00:14 +0300

    Delet HW 7 Lesson

--- HW6a.py
+++
@@ -1,8 +0,0 @@
-from itertools import count
-
-с = 1
-for el in count(int(input("Введите число для начала генерации: "))):
-    if с > 20:
-        break
-    print(el)
-    с += 1
--- HW6b.py
+++
@@ -1,9 +0,0 @@
-from itertools import cycle
-
-с = 1
-my_list = [123, True, "Hello"]
-for el in cycle(my_list):
-    if с > 20:
-        break
-    print(el)
-    с += 1
